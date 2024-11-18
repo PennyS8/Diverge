f3f86dd66fb86e56bf00971350b56a2f92e292a0
n8hopp <hopper26@up.edu>
2024-11-17 21:09:15 -0800

    Added debug scene entry
    When we first load our game, if the name of the scene is not "main", we save the path of the current scene, change the scene to main, and call `_swap_level` on our saved path. This makes it so that we can effectively test any scene with "run current scene" including enemies and objects

--- modules/levels/placeholder_mom_house/stat_replenish.gd
+++ modules/levels/placeholder_mom_house/stat_replenish.gd
@@ -2,6 +2,8 @@ extends Node2D
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
+	# NATE: I added this await line because if we run this from 
+	await LevelManager.main_ready
 	var player = get_tree().get_first_node_in_group("player")
 	print(player.health_component.health)
 	player.health_component.health = player.health_component.max_health
