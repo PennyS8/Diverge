7284daaa14d48827eb1911fb864ad775468ebf1f
n8hopp <hopper26@up.edu>
2025-01-25 02:55:15 -0800

    Enemies are a bit more reactive & smarter
    - Added particle effects to show hits more clearly for a placeholder
    
    - Made Shades roam to random points near them while not chasing the player
    
    - Shades will lose the player if he outruns them

--- modules/entities/enemies/shades/shade.gd
+++ modules/entities/enemies/shades/shade.gd
@@ -1,11 +1,13 @@
 extends CharacterBody2D
 
-@export var hitpoints = 10
+@export var hitpoints = 20
 @export var movement_speed : float = 30.0
 
 @onready var status_holder = get_node("StatusHolder")
 
 var follow_target
+var knockback : Vector2 = Vector2.ZERO
+var crowd_control := false
 
 func _ready() -> void:
 	pass
@@ -18,12 +20,16 @@ func _on_health_component_died() -> void:
 
 func _on_hurt_box_component_2d_hit(_area : HitBoxComponent2D) -> void:
 	if $HealthComponent.health > 0:
+		knockback = _area.global_position.direction_to(global_position) * _area.knockback_coef
 		$ShadeFSM.change_state("Stunned")
+		$CPUParticles2D.restart()
+		
 		# If the attacking _area is the players thread apply the tethered status effect
 		if _area.is_in_group("thread"):
 			status_holder.add_status("tethered")
 	else:
-		print("hi")
+		$ShadeFSM.change_state("Dead")
+
 
 func pull():
 	pass
@@ -31,3 +37,8 @@ func pull():
 	#var tether_dir = player_pos.direction_to(global_position).normalized()
 	#var new_pos = player_pos + tether_dir*THREAD_LENGTH
 	#self.global_position = new_pos
+
+
+func _on_agro_region_body_exited(body):
+	follow_target = null
+	$ShadeFSM.change_state("Roaming")
