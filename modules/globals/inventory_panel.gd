3092a3098b9ba7179fcffda5bc2e0304c17dd7e4
PennyS8 <silliman25@up.edu>
2025-02-14 14:25:41 -0800

    Added collisions to the school_dungeon_dec18

--- modules/globals/inventory_panel.gd
+++ modules/globals/inventory_panel.gd
@@ -1,7 +1,7 @@
 extends CanvasLayer
 
 # Called when the node enters the scene tree for the first time.
-func _input(event: InputEvent) -> void:
+func _input(_event: InputEvent) -> void:
 	if Input.is_action_just_pressed("menu_inventory"):
 		if visible:
 			hide()
--- modules/ui/hud/wyvern_inv/notebook_panel.gd
+++ modules/ui/hud/wyvern_inv/notebook_panel.gd
@@ -1,5 +1,5 @@
 extends Panel
 
 signal check_hook
-func _on_equip_item_stack_added(item_stack: ItemStack) -> void:
+func _on_equip_item_stack_added(_item_stack: ItemStack) -> void:
 	check_hook.emit()
