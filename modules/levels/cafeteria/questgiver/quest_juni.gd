51e41cbf09fa9bf007e2f80e7c5db06be67654bc
n8hopp <hopper26@up.edu>
2025-03-12 18:37:25 -0700

    portraits working in dialogue

--- modules/levels/cafeteria/questgiver/quest_juni.gd
+++ modules/levels/cafeteria/questgiver/quest_juni.gd
@@ -3,5 +3,5 @@ extends StaticBody2D
 
 func _on_interaction_circle_body_entered(body: Node2D) -> void:
 	body.dir = Vector2.ZERO
-	var dialogue = load("res://modules/dialogue/dialogue.dialogue")
-	DialogueManager.show_dialogue_balloon(dialogue, "this_is_a_node_title")
+	var dialogue = load("res://modules/levels/cafeteria/questgiver/juni_hungry.dialogue")
+	DialogueManager.show_dialogue_balloon(dialogue, "start")
