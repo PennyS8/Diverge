81679aba951dd47e7df451dc3782232c9d77323f
PennyS8 <silliman25@up.edu>
2025-03-15 20:26:50 -0700

    Created the stations and dialogue zones

---
+++ modules/levels/school_modular_levels/chem_lab_puzzle/lab_props/mixer_station.gd
@@ -0,0 +1,19 @@
+extends StaticBody2D
+
+var interactable : bool = false
+@onready var player = get_tree().get_first_node_in_group("player")
+
+func _unhandled_input(event: InputEvent) -> void:
+	if not interactable:
+		return
+	
+	if Input.is_action_just_pressed("interact"):
+		player.dir = Vector2.ZERO
+		var dialogue = load("res://modules/levels/school_modular_levels/chem_lab_puzzle/interactions/chem_lab_stations.dialogue")
+		DialogueManager.show_dialogue_balloon(dialogue, "mixer")
+
+func _on_mixer_body_entered(body: Node2D) -> void:
+	interactable = true
+
+func _on_mixer_body_exited(body: Node2D) -> void:
+	interactable = false
