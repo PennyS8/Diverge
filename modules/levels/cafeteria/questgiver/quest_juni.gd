7529034fcc830f25fc64c078fb72e6118425de83
n8hopp <hopper26@up.edu>
2025-03-17 23:10:17 -0700

    some progress on juni puzzle talking

--- modules/levels/cafeteria/questgiver/quest_juni.gd
+++ modules/levels/cafeteria/questgiver/quest_juni.gd
@@ -8,4 +8,12 @@ func _ready():
 func _on_interaction_circle_body_entered(body: Node2D) -> void:
 	body.dir = Vector2.ZERO
 	var dialogue = load("res://modules/levels/cafeteria/questgiver/juni_hungry.dialogue")
-	DialogueManager.show_dialogue_balloon(dialogue, "start")
+	DialogueManager.show_dialogue_balloon(dialogue, "start", [self])
+	
+func _test_if_ramen() -> bool:
+	var inv : Inventory = GameManager.inventory_node.inventory
+	if InventoryHelper.is_itemtype_in_inventory(inv, item_type):
+		inv.consume_items({item_type: 1})
+		return true
+	else:
+		return false
