7793000e69cbdf5abc21c42f102ab7a6d26d884b
n8hopp <hopper26@up.edu>
2025-03-17 22:26:45 -0700

    Wrote some functions that help out a lot with item management

--- modules/levels/cafeteria/questgiver/quest_juni.gd
+++ modules/levels/cafeteria/questgiver/quest_juni.gd
@@ -1,5 +1,9 @@
 extends StaticBody2D
 
+@export var item_type : ItemType
+
+func _ready():
+	InventoryHelper.add_ground_item(item_type, global_position+Vector2(-24, -24), self)
 
 func _on_interaction_circle_body_entered(body: Node2D) -> void:
 	body.dir = Vector2.ZERO
--- modules/objects/inventory_helpers/nates_ground_item.gd
+++ modules/objects/inventory_helpers/nates_ground_item.gd
@@ -9,7 +9,6 @@ func _ready():
 	var stack = ItemStack.new(ground_item, num_item)
 	item_node.item_stack = stack
 
-
 func _on_item_body_entered(body):
 	# adds to juni's main inventory. if want to change this let me know and we'll figure it out
 	var deinv = GameManager.inventory_node.inventory
