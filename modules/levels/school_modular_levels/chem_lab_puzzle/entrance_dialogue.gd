14aecbc281993a7458e457914382f8c0cb45c536
PennyS8 <silliman25@up.edu>
2025-03-18 20:40:37 -0700

    made the entrance dialogue a scene to duplicate

--- modules/levels/school_modular_levels/chem_lab_puzzle/entrance_dialogue.gd
+++ modules/levels/school_modular_levels/chem_lab_puzzle/entrance_dialogue.gd
@@ -8,10 +8,15 @@ func _on_body_entered(_body: Node2D) -> void:
 		return
 	
 	read = true # TODO: "read" must be saved by the save&load manager
-	# This dialogue should be reset every time you leave the room and return
-	# EXCEPT, in the case the puzzle has already been solved
+	# By defulat this dialogue should be reset every time you leave the room
+	# and return. EXCEPT, in the case the puzzle has already been solved; in
+	# this case, save var "read" as true.
 	
 	player.dir = Vector2.ZERO
 	var dialogue = load("res://modules/levels/school_modular_levels/chem_lab_puzzle/interactions/chem_lab_stations.dialogue")
+	# Additionally you can add a dialogue for when the puzzle has been solved to
+	# indicate to the player there is nothing more for them here.
+	# i.e., "Dammit! I forgot what I was doing again. I already [insert task completed in this puzzle],
+	# better retrace my steps and try to remember what I was doing."
 	DialogueManager.show_dialogue_balloon(dialogue, "entrance")
 	
