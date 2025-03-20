7ead37ce3a805452819941bc14a133473a02a491
n8hopp <hopper26@up.edu>
2025-03-19 20:34:31 -0700

    inventory node yeah yeah

--- modules/objects/level_transition_zone/transition_double_door.gd
+++ modules/objects/level_transition_zone/transition_double_door.gd
@@ -5,26 +5,29 @@ var interactable : bool = false
 @export_file("*.tscn","*.scn") var next_level_path
 @export var entrance_name : String
 
+var inv : Inventory
+var count_dict : Dictionary
 @export var keys_needed : Array[ItemLike]
+
+func _ready():
+	# check if we have proper keys
+	if GameManager.inventory_node:
+		inv = GameManager.inventory_node.inventory
+	count_dict = {}
+	for item in keys_needed:
+		count_dict.set(item, 1)
+		
 func _unhandled_input(_event: InputEvent) -> void:
 	if !interactable:
 		return
-	
-	# check if we have proper keys
-	var inv : Inventory = GameManager.inventory_node.inventory
-	var count_dict = {}
-	for item in keys_needed:
-		count_dict.set(item, 1)
-	if !inv.has_items(keys_needed, count_dict):
-		return
+
 	# TODO: check player inventory for 3 books
 	
 	if Input.is_action_just_pressed("interact"):
 		player.dir = Vector2.ZERO
-		LevelManager.change_level(next_level_path, entrance_name)
-		
+
 		var dialogue = load("res://modules/levels/school_modular_levels/Playtest/playtest_dialogue.dialogue")
-		DialogueManager.show_dialogue_balloon(dialogue, "double_door")
+		DialogueManager.show_dialogue_balloon(dialogue, "double_door", [self])
 		
 		get_viewport().set_input_as_handled()
 
@@ -35,3 +38,14 @@ func _on_area_2d_body_entered(body: Node2D) -> void:
 func _on_area_2d_body_exited(body: Node2D) -> void:
 	interactable = false
 	$Glint.hide()
+	
+func check_keys():
+	if !inv:
+		inv = GameManager.inventory_node.inventory
+	
+	if inv.has_items(keys_needed, count_dict):
+		return true
+	else: return false
+	
+func you_shall_pass():
+	LevelManager.change_level(next_level_path, entrance_name)
