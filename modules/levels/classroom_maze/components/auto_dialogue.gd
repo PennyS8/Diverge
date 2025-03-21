c97e8c6341a911590324b101c9ccd28e9fc175b8
n8hopp <hopper26@up.edu>
2025-03-21 15:11:27 -0700

    Enemy stun, fix sophie scene stuff, yay

--- modules/levels/classroom_maze/components/auto_dialogue.gd
+++ modules/levels/classroom_maze/components/auto_dialogue.gd
@@ -4,16 +4,20 @@ var iteration = 0
 
 @onready var playerNoise
 var dialogue = load("res://modules/levels/classroom_maze/anxious_classroom.dialogue")
-
+@onready var camera_location = %CameraLocation
+@export var book : ItemType
 func _on_body_entered(body: Node2D) -> void:
 	if !playerNoise:
 		playerNoise = LevelManager.player.get_node("stressEffect")
 	body.dir = Vector2.ZERO
 	match iteration:
 		0:
+			LevelManager.player.enter_cutscene(camera_location.global_position)
 			DialogueManager.show_dialogue_balloon(dialogue, "start")
 		1:
-			playerNoise.hide()
-			DialogueManager.show_dialogue_balloon(dialogue, "complete")
-			self.queue_free()
+			if InventoryHelper.is_itemtype_in_inventory(GameManager.inventory_node.inventory, book):
+				LevelManager.player.enter_cutscene(camera_location.global_position)
+				playerNoise.hide()
+				DialogueManager.show_dialogue_balloon(dialogue, "complete")
+				self.queue_free()
 	iteration = iteration + 1
