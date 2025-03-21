897def99258c3d2dfd9705d48012ebef7ac178d9
n8hopp <hopper26@up.edu>
2025-03-21 00:42:32 -0700

    Dash fixed and yarn at center of shade

--- modules/entities/player/xsm/can_dash.gd
+++ modules/entities/player/xsm/can_dash.gd
@@ -5,7 +5,18 @@ extends State
 #
 # FUNCTIONS TO INHERIT IN YOUR STATES
 #
+
+func _on_enter(_args):
+	target.unhandled_input_received.connect(state_unhandled_input)
+
 func _on_update(_delta):
+	pass
+
+func state_unhandled_input(event):
 	if Input.is_action_just_pressed("dash"):
+		change_state("Recall")
 		change_state("Dash")
 		change_state("NoAttack")
+		
+func _before_exit(_args) -> void:
+	target.unhandled_input_received.disconnect(state_unhandled_input)
