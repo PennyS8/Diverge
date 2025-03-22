7cf669ced1f0f127e82f129897df53ae57bbf101
n8hopp <hopper26@up.edu>
2025-03-21 17:26:37 -0700

    Pre-export Profiles

--- modules/globals/game_manager.gd
+++ modules/globals/game_manager.gd
@@ -6,7 +6,7 @@ signal last_ramen_picked_up()
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
-	#Input.mouse_mode = Input.MOUSE_MODE_CONFINED
+	Input.mouse_mode = Input.MOUSE_MODE_CONFINED
 	pass
 	
 func _input(event: InputEvent) -> void:
@@ -15,6 +15,6 @@ func _input(event: InputEvent) -> void:
 		
 	if event.is_action_pressed("left_click"):
 		if Input.mouse_mode == Input.MOUSE_MODE_VISIBLE:
-			#Input.mouse_mode = Input.MOUSE_MODE_CONFINED
+			Input.mouse_mode = Input.MOUSE_MODE_CONFINED
 			return
 	pass
