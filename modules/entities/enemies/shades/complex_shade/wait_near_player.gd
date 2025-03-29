a811cd365994701336c9cc773fe37edcfe32210c
n8hopp <hopper26@up.edu>
2025-03-28 22:32:04 -0700

    enemy picks move on enter

--- modules/entities/enemies/shades/complex_shade/wait_near_player.gd
+++ modules/entities/enemies/shades/complex_shade/wait_near_player.gd
@@ -1,7 +1,8 @@
 @tool
 extends State
 
-@export var desired_distance : int
+@export var desired_distance : float
+@export var personal_bubble_radius : float
 @export var state_speed : float
 @onready var nav_agent = %NavAgent
 #
@@ -12,6 +13,7 @@ extends State
 # XSM enters the root first, the the children
 func _on_enter(_args):
 	target.movement_speed = state_speed
+	_pick_next_move()
 
 
 # This function is called just after the state enters
@@ -23,24 +25,40 @@ func _after_enter(_args):
 # This function is called each frame if the state is ACTIVE
 # XSM updates the root first, then the children
 func _on_update(_delta):
-	var enemy_position : Vector2 = target.global_position
-	var player_position : Vector2 = target.follow_object.global_position
-	var player_distance : float = player_position.distance_to(enemy_position)
-	if player_distance < desired_distance:
-		# desired location is straight backward +- 20degrees
-		var desired_location = player_position.direction_to(enemy_position).normalized()
-		desired_location = desired_location * (desired_distance-player_distance)
-		target.set_movement_goal(target.to_local(desired_location))
-
-	else:
-		# desired location a point on circle of radius=desired_distance from player
-		pass
-	
-
 	# if we've reached our desired point
 	if nav_agent.is_navigation_finished():
 		# chase checks if we can engage; if not, it transitions back to this state
 		change_state("Chase")
+	
+func _pick_next_move():
+	var enemy_position : Vector2 = target.global_position
+	var player_position : Vector2 = target.follow_object.global_position
+	var player_distance : float = player_position.distance_to(enemy_position)
+	# if player is within personal bubble, then walk backwards to desired_distance away
+	if player_distance < personal_bubble_radius:
+		# desired location is straight backward --- eventually +- 20degrees, not impl. yet
+		var desired_location = player_position.direction_to(enemy_position).normalized()
+		desired_location = desired_location * desired_distance
+		target.set_movement_goal(target.to_global(desired_location))
+	# else, player is outside of personal bubble, so do dynamic movement instead
+	else:
+		# desired location a point on circle of radius=desired_distance from player
+		var angle_relative_player = player_position.angle_to_point(enemy_position)
+		var coin_flip = randi_range(0, 1)
+		var angle_shift : int
+		match coin_flip:
+			0:
+				# 20 degrees in radians
+				angle_shift = -0.349066
+			1:
+				angle_shift = 0.349066
+		
+		var new_angle : float = angle_relative_player + (angle_shift)
+		var new_pos : Vector2 = player_position.from_angle(new_angle) * desired_distance
+		target.set_movement_goal(target.follow_object.to_global(new_pos))
+
+
+	
 
 
 # This function is called each frame after all the update calls
