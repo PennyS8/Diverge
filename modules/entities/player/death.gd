4dc77ff46060aebbad680141ab1f2b2fe732289e
PennyS8 <silliman25@up.edu>
2025-04-16 19:38:58 -0700

    Revert "bugfix dying while moving diagonal"
    This reverts commit 1fa2f8d9dfbb4e1ddff43c97803b66f3d8c79319.

--- modules/entities/player/death.gd
+++ modules/entities/player/death.gd
@@ -7,7 +7,7 @@ var death_states : Dictionary = {
 	Vector2.DOWN: "DeathDown",
 	Vector2.LEFT: "DeathLeft"
 	}
-
+	
 var death_dir : Vector2
 
 func _on_enter(_args) -> void:
@@ -18,14 +18,5 @@ func _on_enter(_args) -> void:
 		death_dir = _args
 	else:
 		death_dir = Vector2.DOWN
-	
-	# Take the direction the player is facing and lock it to a cardinal 
-	var component_x = abs(death_dir.x)
-	var component_y = abs(death_dir.y)
-	if component_x >= component_y:
-		death_dir.y = 0
-	else:
-		death_dir.x = 0
-	death_dir = death_dir.snapped(Vector2.ONE)
 	
 	change_state(death_states[death_dir])
