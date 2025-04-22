57e6d5b3e3bf6df92f5bc57c44b39d1697c37ccb
PennyS8 <silliman25@up.edu>
2025-04-22 00:13:16 -0700

    polish

---
+++ modules/levels/school_modular_levels/school_dungeon_act_one/attack_tutorial_entrance_dialogue.gd
@@ -0,0 +1,17 @@
+extends Area2D
+
+var read : bool = false
+@onready var shade = $"../ComplexShade"
+@onready var player = get_tree().get_first_node_in_group("player")
+
+func _on_body_entered(_body: Node2D) -> void:
+	if read: # Don't allow this dialogue to be read more than once
+		return
+	
+	read = true
+	
+	shade.get_node("ShadeFSM").disabled = true
+	player.dir = Vector2.ZERO
+	
+	var dialogue = load("res://modules/levels/school_modular_levels/tutorials/melee_attack_tutorial.dialogue")
+	await DialogueManager.show_dialogue_balloon(dialogue, "start")
--- modules/levels/school_modular_levels/school_dungeon_act_one/attack_tutorial_scene.gd
+++ modules/levels/school_modular_levels/school_dungeon_act_one/attack_tutorial_scene.gd
@@ -2,7 +2,6 @@ extends Node2D
 
 @onready var shade = $ComplexShade
 @onready var player = get_tree().get_first_node_in_group("player")
-@onready var dialogue = load("res://modules/levels/school_modular_levels/tutorials/melee_attack_tutorial.dialogue")
 
 var read : bool = false
 
@@ -11,6 +10,4 @@ func _physics_process(delta: float) -> void:
 		return
 	elif player.global_position.distance_to(shade.global_position) <= 48:
 		read = true
-		
-		#DialogueManager.show_dialogue_balloon(dialogue, "start")
 		LevelManager.enter_tutorial("AttackTutorial")
