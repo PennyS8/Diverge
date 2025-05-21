c3540fd3c536dd28cfcf7edc958342c7d79ac9b7
n8hopp <hopper26@up.edu>
2025-05-20 21:09:42 -0700

    QoL Changes
    - Added an indicator in coping mechanism for "main shade" vs "target shades". If main shade, shows a little orb. If not main shade, it shows an arrow that points to the main shade, indicating where it goings.
    - Fixed a bug where the lasso from boss shade to its minions was overly sized
    - Fixed a bug where player would slide along the ground when death occurs

--- modules/ui/coping_overlays/selection_indicator.gd
+++ modules/ui/coping_overlays/selection_indicator.gd
@@ -1,4 +1,16 @@
 extends Line2D
 
-func _on_visibility_changed():
-	pass
+@onready var arrow := $Arrow
+@onready var primary := $Primary
+
+func set_indicator(is_first, direction := Vector2.UP):
+	if is_first:
+		primary.show()
+		arrow.hide()
+	else:
+		primary.hide()
+		arrow.show()
+		
+		# We add a quarter rotation to the angle since the arrow's default direction is up rather than right
+		# This effectively acts as an offset to reset the arrow's rotation to match the radian direction
+		arrow.rotate(get_angle_to(to_global(direction)) + (PI / 2))
